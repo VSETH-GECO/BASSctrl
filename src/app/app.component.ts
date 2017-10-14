d42f8b9f39fc3b77886ecfdc08ac0702c6e6c17b
Bermos <matthew.machivenyika@hispeed.ch>
2017-10-14 21:04:39 +0200

    I have no clue how angular works but this should establish a websocket connection on app init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {Track} from './track';
+import {WebsocketService} from './websocket.service';
 
 const track: Track = {id: 1, title: 'Schostakovich 5', submitter: null};
 
@@ -7,9 +8,10 @@ const track: Track = {id: 1, title: 'Schostakovich 5', submitter: null};
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [WebsocketService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'BASS control';
 
   // Player, should be refactored into it's own component later on
@@ -18,9 +20,22 @@ export class AppComponent {
 
   onPlayerMethod(): void {
     if (this.currentMethod === 'Play') {
+
       this.currentMethod = 'Pause';
     } else {
       this.currentMethod = 'Play';
     }
+  }
+
+
+  // This plus onInit should connect the websocket
+  constructor(private websocketService: WebsocketService) { }
+  connectToWebsocket(): void {
+    this.websocketService.connect('ws://localhost:8080');
+  }
+
+  // on init of the app
+  ngOnInit(): void {
+    this.connectToWebsocket();
   }
 }
--- src/app/websocket.service.ts
+++ src/app/websocket.service.ts
@@ -1,39 +1,28 @@
-import Rx from 'rxjs/Rx';
+import {$WebSocket, WebSocketSendMode} from 'angular2-websocket/angular2-websocket';
 import {Injectable} from '@angular/core';
 
 @Injectable()
 export class WebsocketService {
-  private socket: Rx.Subject<MessageEvent>
+  private socket: $WebSocket;
 
-  public connect(url): Rx.Subject<MessageEvent> {
+  public connect(url): boolean {
     if (!this.socket) {
-      this.socket = this.create(url);
+      this.socket = new $WebSocket(url);
     }
 
-    return this.socket;
+    return this != null;
   }
 
-  private create(url): Rx.Subject<MessageEvent> {
-    let ws = new WebSocket(url);
-
-    let observable = Rx.Observable.create(
-      (obs: Rx.Observer<MessageEvent>) => {
-        ws.onmessage = obs.next.bind(obs);
-        ws.onerror = obs.error.bind(obs);
-        ws.onclose = obs.complete.bind(obs);
-
-        return ws.close.bind(ws);
-      }
+  public send(payload): boolean {
+    this.socket.send(payload, WebSocketSendMode.Promise).then(
+      () => {
+        return true;
+      },
+      () => {
+        return false;
+    }
     );
 
-    let observer = {
-      next: (data: Object) => {
-        if (ws.readyState === WebSocket.OPEN) {
-          ws.send(JSON.stringify(data));
-        }
-      },
-    };
-
-    return Rx.Subject.create(observer, observable);
+    return false;
   }
 }

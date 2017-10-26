d17eb4de3f7790707b507c8161828825d510eb00
Bermos <matthew.machivenyika@hispeed.ch>
2017-10-26 02:26:10 +0200

    Added login function

--- src/app/login.component.ts
+++ src/app/login.component.ts
@@ -1,8 +1,28 @@
-import {Component} from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {ServerCtrlService} from './server-ctrl.service';
+import {WsPackage} from './ws-package';
 
 @Component({
   selector: 'login',
   templateUrl: '/login.component.html',
 })
 export class LoginComponent {
+  @Input() username: string;
+  @Input() password: string;
+
+  constructor(private serverCtrlService: ServerCtrlService) {}
+
+  login(): void {
+    if (this.username && this.password) {
+
+      this.serverCtrlService.wsPackages.next(
+        new WsPackage('post', 'user/login', {
+          username: this.username,
+          password: this.password
+        })
+      );
+    } else {
+      alert('Enter both username and password to proceed');
+    }
+  }
 }
